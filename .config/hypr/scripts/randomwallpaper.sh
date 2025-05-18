b0713cfe2a55056145594912d0b3e161a052d13a
Viet Phung <phungquocviet0608@gmail.com>
2025-05-19 03:09:50 +0700

    Add file wallpaper_select and wallpaper_effects

--- .config/hypr/scripts/randomwallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-src_dir="$HOME/Pictures/Wallpapers"
-des_dir="$HOME/.cache/swww"
-
-swww img $src_dir/$(ls $src_dir | shuf -n1) --transition-type any --transition-duration 2
-rm -rf $des_dir/*.png
-vipsthumbnail $(cat $des_dir/eDP-1) -o $des_dir/normal.png
