720d073a9fd8ae733dae1c273d49b2c1e22d4802
Viet Phung <phungquocviet0608@gmail.com>
2025-05-29 22:25:02 +0700

    Update installpkg.sh

--- .config/hypr/scripts/installfonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
-unzip JetBrainsMono.zip
-fc-cache -fv
-rm JetBrainsMono.zip
-cd ~
