372acdd30327729c8c0b08f370e030469c790f4f
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-06-24 01:31:54 +0700

    Update vim-options.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,8 +1,9 @@
 -- Setup Neovim
-vim.opt.number = true       -- Display line numbers
-vim.opt.wrap = false        -- Disable wrap line
-vim.opt.sidescroll = 1      -- Scroll 1-char horizontally
-vim.opt.sidescrolloff = 5   -- Keep 5-char margin
+vim.opt.number = true                   -- Display line numbers
+vim.opt.fillchars:append { eob = " " }  -- Hide '~' on empty buffer lines
+vim.opt.wrap = false                    -- Disable wrap line
+vim.opt.sidescroll = 1                  -- Scroll 1-char horizontally
+vim.opt.sidescrolloff = 5               -- Keep 5-char margin
 -- Set tab = 4 space
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
@@ -19,4 +20,3 @@ vim.keymap.set("n", "<A-Up>", ":m .-2<CR>", {})            -- Move line in Norma
 -- Set leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
