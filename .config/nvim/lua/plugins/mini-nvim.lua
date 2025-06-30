e4c16d52091b5221e7a92ba3461704a17d0de897
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-06-30 19:18:34 +0700

    Update mini-nvim.lua

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -10,7 +10,6 @@ return {
         comment.setup({
             ignore_blank_line = true,
         })
-        -- <C-_> is Ctrl + /
         vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle Comment (Normal mode)" })    -- Space /
         vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle Comment (Visual mode)" })     -- Space /
     end
