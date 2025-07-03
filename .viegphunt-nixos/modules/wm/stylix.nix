62200cba1aef90768fcd2c4910ce20627ee34738
Viet Phung <phungquocviet0608@gmail.com>
2025-07-04 05:28:03 +0700

    Fix bug

--- .viegphunt-nixos/modules/wm/stylix.nix
+++ .viegphunt-nixos/modules/wm/stylix.nix
@@ -2,5 +2,38 @@
     stylix = {
         enable = true;
         autoEnable = false;
+        polarity = "dark";
+
+        targets = {
+            gtk.enable = true;
+            cursors.enable = true;
+        };
+
+        cursor = {
+            package = pkgs.apple-cursor;
+            name = "macOS";
+            size = 24;
+        };
+
+        gtk = {
+            theme = {
+                package = pkgs.adw-gtk3;
+                name = "adw-gtk3-dark";
+            };
+            iconTheme = {
+                package = pkgs.whitesur-icon-theme;
+                name = "WhiteSur-dark";
+            };
+        };
+
+        fonts = {
+            sansSerif = {
+                package = pkgs.adwaita-fonts;
+                name = "Adwaita Sans";
+            };
+            sizes.applications = 12;
+        };
     };
+
+    gtk.enable = true;
 }
