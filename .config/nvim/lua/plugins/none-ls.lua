23e18116efd76efa4ff4e31a36491e60db9dc675
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-07-04 15:42:28 +0700

    Update none-ls.lua

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -2,12 +2,16 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
+        local formatting = null_ls.builtins.formatting
+
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,         -- Lua
-                null_ls.builtins.formatting.clang_format,   -- C/C++
-                null_ls.builtins.formatting.black,          -- Python
-                null_ls.builtins.formatting.prettier,       -- JavaScript
+                formatting.stylua,              -- Lua
+                formatting.clang_format,        -- C/C++
+                formatting.black,               -- Python
+                formatting.prettier,            -- JavaScript, Html, Css
+                formatting.google_java_format,  -- Java
+                formatting.sql_formatter,       -- SQL
             }
         })
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = "Format Code" })    -- Space g f
