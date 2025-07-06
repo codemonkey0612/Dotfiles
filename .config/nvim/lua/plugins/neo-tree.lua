30cbca8635c6d6f718c647d3d0da7d8adc43f042
Viet Phung <phungquocviet0608@gmail.com>
2025-07-06 16:21:22 +0700

    Add noremap nvim

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -19,6 +19,10 @@ return {
                 width = 25, -- Set width window neotree (default 40)
             },
         })
-        vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', { desc = "Open File Explorer", silent = true })    -- Space e
+        vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {
+            noremap = true,
+            desc = "Open File Explorer",
+            silent = true
+        })  -- Space e
     end
 }
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,11 @@ return {
         require("toggleterm").setup({
             start_in_insert = false
         })
-        vim.keymap.set('n', '<leader>`', ':ToggleTerm size=10 direction=horizontal<CR>', { desc = "Open Bottom Terminal", silent = true }) -- Space `
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {})
+        vim.keymap.set('n', '<leader>`', ':ToggleTerm size=10 direction=horizontal<CR>', {
+            noremap = true,
+            desc = "Open Bottom Terminal",
+            silent = true
+        })  -- Space `
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { noremap = true })
     end
 }
