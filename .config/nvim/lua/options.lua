991d23c1ba275426193d21e32388e3dcdfc4d527
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-07-18 22:07:49 +0700

    Update options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,18 +1,28 @@
--- Setup Neovim
+-- Setup Options Neovim
+
+vim.opt.laststatus = 3
+vim.opt.showmode = false
+vim.opt.undofile = true
+vim.opt.whichwrap:append "<>[]hl"       -- Allow left/right keys to wrap to the next/previous line
+
+-- Line numbers
 vim.opt.number = true                   -- Display line numbers
-vim.opt.numberwidth = 3                 -- Set min number column width
+vim.opt.numberwidth = 2                 -- Set min number column width
+
+-- Display and UI
 vim.opt.cursorline = true               -- Highlight cursor line
-vim.opt.fillchars:append { eob = " " }  -- Hide '~' on empty buffer lines
+vim.opt.fillchars = { eob = " " }       -- Hide '~' on empty buffer lines
 vim.opt.wrap = false                    -- Disable wrap line
 vim.opt.sidescroll = 1                  -- Scroll 1-char horizontally
 vim.opt.sidescrolloff = 5               -- Keep 5-char margin
-vim.opt.undofile = true
-vim.opt.winborder = "rounded"
 vim.opt.signcolumn = "yes"
+vim.opt.winborder = "rounded"
+
 -- Set tab = 4 space
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
 -- Set color
 vim.opt.termguicolors = true
