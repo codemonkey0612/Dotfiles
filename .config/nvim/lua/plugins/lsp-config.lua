c55cb5e3dd65f43ac2f8fff305fe7098f1f59087
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-08-02 21:32:37 +0700

    Update lsp-config.lua

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,41 +1,43 @@
 return {
     {
-        "mason-org/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         dependencies = {
             "mason-org/mason.nvim",
-            "neovim/nvim-lspconfig",
+            "mason-org/mason-lspconfig.nvim",
         },
 
         config = function()
             require("mason").setup({})
 
+            -- Lua, C/C++, Python, JavaScript, Java, Html, Css, SQL
+            local servers = { "lua_ls", "clangd", "pyright", "ts_ls", "jdtls", "html", "cssls", "sqlls" }
             require("mason-lspconfig").setup({
-                -- Lua, C/C++, Python, JavaScript, Java, Html, Css, SQL
-                ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "jdtls", "html", "cssls", "sqlls" },
+                ensure_installed = servers,
             })
 
             vim.lsp.config('lua_ls', {
                 settings = {
                     Lua = {
-                        runtime = { version = 'LuaJIT' },
                         diagnostics = { globals = { 'vim', 'require' } },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
+                        workspace = { checkThirdParty = false },
                         telemetry = { enable = false },
                     },
                 },
             })
+            vim.lsp.enable(servers)
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Hover Documentation" })              -- Shift k
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go To Definition" })                -- g d
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Trigger Code Action" })    -- Space c a
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show Hover Documentation" })              -- Shift k
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, desc = "Go To Definition" })                -- g d
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "Trigger Code Action" })    -- Space c a
+                end,
+            })
+
+            vim.diagnostic.config({ virtual_text = true, signs = true, underline = true, update_in_insert = false })
         end
     },
 
     -- Java
-    {
-        "mfussenegger/nvim-jdtls";
-    }
+    { "mfussenegger/nvim-jdtls" }
 }
