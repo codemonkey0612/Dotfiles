a50a36ab97ee07c7aed10b6b100db9bca014ca47
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-08-02 22:52:06 +0700

    Update keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -19,10 +19,10 @@ keymap("n", "<A-,>", "<C-w>>", { desc = "Make Window Wider" })      -- Alt ,
 keymap("n", "<A-.>", "<C-w><", { desc = "Make Window Narrower" })   -- Alt .
 
 -- Move on pane
-keymap("n", "<C-K>", "<C-w>k", { desc = "Move To Upper Pane" })         -- Ctrl k
-keymap("n", "<C-J>", "<C-w>j", { desc = "Move To Bottom Pane" })        -- Ctrl j
-keymap("n", "<C-H>", "<C-w>h", { desc = "Move To Left Pane" })          -- Ctrl h
-keymap("n", "<C-L>", "<C-w>l", { desc = "Move To Right Pane" })         -- Ctrl l
+keymap("n", "<C-K>", "<C-w>k", { desc = "Move To Upper Pane" })     -- Ctrl k
+keymap("n", "<C-J>", "<C-w>j", { desc = "Move To Bottom Pane" })    -- Ctrl j
+keymap("n", "<C-H>", "<C-w>h", { desc = "Move To Left Pane" })      -- Ctrl h
+keymap("n", "<C-L>", "<C-w>l", { desc = "Move To Right Pane" })     -- Ctrl l
 
 -- Set move line
 keymap("n", "<A-Up>", ":m .-2<CR>", { desc = "Move Line Up (Normal mode)", silent = true })                 -- Alt Up
