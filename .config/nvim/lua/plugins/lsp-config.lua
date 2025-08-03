fbc6bd0ade471def86931ca48229101d900e1a32
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-08-04 04:57:43 +0700

    Update lsp-config.lua

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,39 +1,47 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
+    {
         "mason-org/mason.nvim",
-        "mason-org/mason-lspconfig.nvim",
-        -- Java
-        "mfussenegger/nvim-jdtls",
+
+        config = function()
+            require("mason").setup({})
+        end
     },
 
-    config = function()
-        require("mason").setup({})
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "mason-org/mason-lspconfig.nvim",
+        },
 
-        -- Lua, C/C++, Python, JavaScript, HTML, CSS, SQL
-        local servers = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls", "sqlls" }
-        require("mason-lspconfig").setup({
-            ensure_installed = servers,
-        })
+        config = function()
+            -- Lua, C/C++, Python, JavaScript, HTML, CSS, SQL
+            local servers = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls", "sqlls" }
+            require("mason-lspconfig").setup({
+                ensure_installed = servers,
+            })
 
-        vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
-        vim.lsp.config("lua_ls", {
-            settings = {
-                Lua = {
-                    diagnostics = { globals = { "vim", "require" } },
-                    workspace = { checkThirdParty = false },
-                    telemetry = { enable = false },
+            vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim", "require" } },
+                        workspace = { checkThirdParty = false },
+                        telemetry = { enable = false },
+                    },
                 },
-            },
-        })
-        vim.lsp.enable(servers)
+            })
+            vim.lsp.enable(servers)
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(event)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show Hover Documentation" })              -- Shift k
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, desc = "Go To Definition" })                -- g d
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "Trigger Code Action" })    -- Space c a
-            end,
-        })
-    end
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show Hover Documentation" })              -- Shift k
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, desc = "Go To Definition" })                -- g d
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "Trigger Code Action" })    -- Space c a
+                end,
+            })
+        end
+    },
+
+    -- Java
+    { "mfussenegger/nvim-jdtls" }
 }
