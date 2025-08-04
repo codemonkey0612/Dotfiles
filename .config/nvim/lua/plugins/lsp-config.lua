c5e6ca15d5a3db82230c697cc0c8f6a3fce63f76
Phùng Quốc Việt <tajdine.elm@gmail.com>
2025-08-04 15:15:16 +0700

    Update lsp-config.lua

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,34 @@
 return {
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            "mason-org/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
-
-        config = function()
-            require("mason").setup({})
-
-            require("mason-lspconfig").setup({
-                -- Lua, C/C++, Python, JavaScript, HTML, CSS, Java, SQL
-                ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls", "jdtls", "sqlls" },
-                automatic_enable = true
-            })
-
-            vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
-            vim.lsp.config("lua_ls", {
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim", "require" } },
-                        workspace = { checkThirdParty = false },
-                        telemetry = { enable = false },
-                    },
-                },
-            })
-
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(event)
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show Hover Documentation" })              -- Shift k
-                    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = event.buf, desc = "Open Diagnostic Float" }) -- Space d
-                end,
-            })
-        end
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
     },
 
-    -- Java
-    { "mfussenegger/nvim-jdtls" }
+    config = function()
+        require("mason").setup({})
+
+        require("mason-lspconfig").setup({
+            -- Lua, C/C++, Python, JavaScript, HTML, CSS, Java, SQL
+            ensure_installed = { "lua_ls", "clangd", "pyright", "ts_ls", "html", "cssls", "java-language-server", "sqlls" },
+            automatic_enable = true
+        })
+
+        vim.lsp.config("*", { capabilities = vim.lsp.protocol.make_client_capabilities() })
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = { globals = { "vim", "require" } },
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                },
+            },
+        })
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(event)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show Hover Documentation" })  -- Shift k
+            end,
+        })
+    end
 }
